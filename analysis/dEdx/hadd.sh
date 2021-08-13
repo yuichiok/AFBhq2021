e651f8ff3e89d1b4a5babac86b07be30c353187e
A. Irles <adrian.irles@ific.uv.es>
2021-08-13 16:25:59 +0200

    Add gitignore

---
+++ analysis/dEdx/hadd.sh
@@ -0,0 +1,8 @@
+for cut in "0" "1" "2" "4" "5"
+do
+    for pol in "eL_pR" "eR_pL"
+    do
+	hadd -f results/histos_cquark_secondary_tracks_ignoreoverlay_2f_hadronic_sample_${pol}_${cut}GeV.root output/*secondary_tracks_ignoreoverlay_2f_hadronic_sample_${pol}*cut_${cut}_cquark.root
+	hadd -f results/histos_bquark_secondary_tracks_ignoreoverlay_2f_hadronic_sample_${pol}_${cut}GeV.root output/*secondary_tracks_ignoreoverlay_2f_hadronic_sample_${pol}*cut_${cut}_bquark.root
+    done
+done
