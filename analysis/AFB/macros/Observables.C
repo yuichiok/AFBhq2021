8bf9355d4a49ce000861477255feb0ef5fbd9f26
airqui <airqui@ific.uv.es>
2022-08-05 11:03:33 +0200

    LO observable calculation for draft

--- analysis/AFB/macros/Observables.C
+++ analysis/AFB/macros/Observables.C
@@ -82,7 +82,7 @@ void Plots_AFB(int mode=1) {
   float luminosity=0;
   float luminosity_eR=0;
 
-  for(int i=2; i<36; i+=1) {
+  for(int i=2; i<110; i+=1) {
 
     if(i==2) {
       TH1F* total = (TH1F*)f->Get("h_Ntotal_nocuts");
@@ -137,7 +137,7 @@ void Plots_AFB(int mode=1) {
   TCanvas* c_AFBb = new TCanvas("c_AFBb","c_AFBb",800,800);
   c_AFBb->cd(1);
   // gPad->SetLogy();
-  gAFBb->GetXaxis()->SetTitle("K_{#gamma}-cut");
+  gAFBb->GetXaxis()->SetTitle("E_{#gamma}^{cut}");
   gAFBb->GetYaxis()->SetTitle("AFB [%]");
   gAFBb->GetYaxis()->SetRangeUser(-40,90);
   gAFBb->SetLineColor(4);
@@ -171,8 +171,8 @@ void Plots_AFB(int mode=1) {
   if(mode==1) {
     pol_string1 = "e^{-}e^{+}";
     pol_string2 = "e^{-}e^{+}";
-    string1 = ", P(-80%,+30%)";
-    string2 = ", P(+80%,-30%)";
+    string1 = ", P_{e^{-}e^{+}}=(-0.8,+0.3)";
+    string2 = ", P_{e^{-}e^{+}}=(+0.8,-0.3)";
   }
   
   TLegend *leg_b = new TLegend(0.2,0.25,0.5,0.45);
@@ -293,7 +293,7 @@ void Plots_AFB2(int mode=1) {
   TCanvas* c_AFBb = new TCanvas("c_AFBb","c_AFBb",800,800);
   c_AFBb->cd(1);
   // gPad->SetLogy();
-  gAFBb->GetXaxis()->SetTitle("sin(#Psi_{acol})-cut");
+  gAFBb->GetXaxis()->SetTitle("sin(#Psi_{acol})^{cut}");
   gAFBb->GetYaxis()->SetTitle("AFB [%]");
   gAFBb->GetYaxis()->SetRangeUser(-40,90);
   gAFBb->SetLineColor(4);
@@ -316,7 +316,7 @@ void Plots_AFB2(int mode=1) {
   gAFBc_eR->SetLineStyle(2);
   gAFBc_eR->Draw("lp");
 
-  Labels(", K_{#gamma}<35 GeV");
+  Labels(", E_{#gamma}<35 GeV");
 
   TString pol_string1 = "e_{L}^{-}e_{R}^{+}";
   TString pol_string2 = "e_{R}^{-}e_{L}^{+}";
@@ -328,8 +328,8 @@ void Plots_AFB2(int mode=1) {
   if(mode==1) {
     pol_string1 = "e^{-}e^{+}";
     pol_string2 = "e^{-}e^{+}";
-    string1 = ", P(-80%,+30%)";
-    string2 = ", P(+80%,-30%)";
+    string1 = ", P_{e^{-}e^{+}}=(-0.8,+0.3)";
+    string2 = ", P_{e^{-}e^{+}}=(+0.8,-0.3)";
   }
   TLegend *leg_b = new TLegend(0.2,0.25,0.5,0.45);
   leg_b->SetTextSize(0.035);
@@ -445,6 +445,9 @@ void Plots_CrossDiff(int mode=0) {
     h2->Add(h2_eR);
     gAFBc_eR = new TGraph(h2);
     delete h2, h2_eR;
+
+    test = new TGraph(temp_AFBc);
+    test2 = new TGraph(temp_AFBc); 
   }
 
 
@@ -454,7 +457,7 @@ void Plots_CrossDiff(int mode=0) {
   TCanvas* c_AFBb = new TCanvas("c_AFBb","c_AFBb",800,800);
   c_AFBb->cd(1);
   // gPad->SetLogy();
-  gAFBb->GetXaxis()->SetTitle("cos(#theta_{thrust})");
+  gAFBb->GetXaxis()->SetTitle("cos(#theta_{q})");
   gAFBb->GetYaxis()->SetTitle("cross section [fb] / 0.05");
   gAFBb->GetYaxis()->SetRangeUser(0,500);
   gAFBb->GetXaxis()->SetRangeUser(-1,1);
@@ -486,7 +489,7 @@ void Plots_CrossDiff(int mode=0) {
   test2->SetLineWidth(2);
   test2->SetLineStyle(2);
 
-  Labels(", sin(#Psi_{acol})<0.3 & K_{#gamma}<35 GeV");
+  Labels(", sin(#Psi_{acol})<0.3 & E_{#gamma}<35 GeV");
 
 
   TString pol_string1 = "e_{L}^{-}e_{R}^{+}";
@@ -498,8 +501,8 @@ void Plots_CrossDiff(int mode=0) {
   if(mode==1) {
     pol_string1 = "e^{-}e^{+}";
     pol_string2 = "e^{-}e^{+}";
-    string1 = ", P(-80%,+30%)";
-    string2 = ", P(+80%,-30%)";
+    string1 = ", P_{e^{-}e^{+}}=(-0.8,+0.3)";
+    string2 = ", P_{e^{-}e^{+}}=(+0.8,-0.3)";
   }
   
   TLegend *leg_b = new TLegend(0.2,0.72,0.5,0.91);
@@ -518,6 +521,205 @@ void Plots_CrossDiff(int mode=0) {
   c_AFBb->Print(TString::Format("plots_draft/DiffCrossSection_acolcut_kcut_mode%i.eps",mode));
 
 }
+
+void Plots_Cross0(int mode=0) {
+
+  //if mode == 0 --> pure polarization assumed
+  //if mode == 1 --> 80,30 polarisations
+   
+  SetQQbarStyle();
+  gStyle->SetOptFit(0); 
+  gStyle->SetOptStat(0);
+  gStyle->SetOptTitle(0);
+  
+  gStyle->SetTitleBorderSize(0);
+  gStyle->SetTitleStyle(0);
+  gStyle->SetTitleX(0.2);
+  gStyle->SetMarkerSize(0.2);
+  // TGaxis::SetMaxDigits(2);
+
+  TString pol="eL_pR";
+  TString filename = "../results/AFBenergyDep_2f_hadronic_sample_"+pol+"_250GeV.root";
+  TFile *f = new TFile(filename);
+
+  TString pol2="eR_pL";
+  TString filename2 = "../results/AFBenergyDep_2f_hadronic_sample_"+pol2+"_250GeV.root";
+  TFile *f2 = new TFile(filename2);
+
+  double AFBb[145]={0};
+  double AFBb_eR[145]={0};
+  double dAFBb[145]={0};
+  double dAFBb_eR[145]={0};
+
+  double AFBc[145]={0};
+  double AFBc_eR[145]={0};
+  double dAFBc[145]={0};
+  double dAFBc_eR[145]={0};
+
+  double AFBq[145]={0};
+  double AFBq_eR[145]={0};
+  double dAFBq[145]={0};
+  double dAFBq_eR[145]={0};
+
+  double x[145]={0};
+  double ex[145]={0};
+  int n=0;
+
+  double totalev=0;
+  double totalev_eR=0;
+  double luminosity=0;
+  double luminosity_eR=0;
+
+  for(int i=2; i<110; i+=1) {
+   if(i==2) {
+      TH1F* total = (TH1F*)f->Get("h_Ntotal_nocuts");
+      TH1F* total_eR = (TH1F*)f2->Get("h_Ntotal_nocuts");
+      totalev=total->Integral();
+      totalev_eR=total_eR->Integral();
+      luminosity=totalev/cross_section[0][3];
+      luminosity_eR=totalev_eR/cross_section[1][3];
+    }
+
+    TH1F* temp_AFBb = (TH1F*)f->Get(TString::Format("h_AFBb_KLowerThan%i",i));
+    AFBb[n]=temp_AFBb->Integral()/luminosity;
+    dAFBb[n]=0;
+
+    TH1F* temp_AFBb_eR = (TH1F*)f2->Get(TString::Format("h_AFBb_KLowerThan%i",i));
+    AFBb_eR[n]=temp_AFBb_eR->Integral()/luminosity_eR;
+    dAFBb_eR[n]=0;
+
+    float temp=AFBb[n];
+    float temp_eR=AFBb_eR[n];
+    if(mode==1) {
+      AFBb[n]=temp*0.58+0.035*temp_eR;
+      AFBb_eR[n]=temp*0.035+0.58*temp_eR;
+    }
+
+    TH1F* temp_AFBc = (TH1F*)f->Get(TString::Format("h_AFBc_KLowerThan%i",i));
+    AFBc[n]=temp_AFBc->Integral()/luminosity;
+    dAFBc[n]=0;
+
+    TH1F* temp_AFBc_eR = (TH1F*)f2->Get(TString::Format("h_AFBc_KLowerThan%i",i));
+    AFBc_eR[n]=temp_AFBc_eR->Integral()/luminosity_eR;
+    dAFBc_eR[n]=0;
+
+    temp=AFBc[n];
+    temp_eR=AFBc_eR[n];
+    if(mode==1) {
+      AFBc[n]=temp*0.58+0.035*temp_eR;
+      AFBc_eR[n]=temp*0.035+0.58*temp_eR;
+    }
+
+    TH1F* temp_AFBq = (TH1F*)f->Get(TString::Format("h_AFBuds_KLowerThan%i",i));
+    AFBq[n]=temp_AFBq->Integral()/luminosity;
+    dAFBq[n]=0;
+
+    TH1F* temp_AFBq_eR = (TH1F*)f2->Get(TString::Format("h_AFBuds_KLowerThan%i",i));
+    AFBq_eR[n]=temp_AFBq_eR->Integral()/luminosity_eR;
+    dAFBq_eR[n]=0;
+
+    temp=AFBq[n];
+    temp_eR=AFBq_eR[n];
+    if(mode==1) {
+      AFBq[n]=temp*0.58+0.035*temp_eR;
+      AFBq_eR[n]=temp*0.035+0.58*temp_eR;
+    }
+
+    x[n]=i;
+    n++;
+
+  }
+
+ 
+  TGraphErrors* gAFBb = new TGraphErrors(n,x,AFBb,ex,dAFBb);
+  TGraphErrors* gAFBb_eR = new TGraphErrors(n,x,AFBb_eR,ex,dAFBb_eR); 
+  TGraphErrors* gAFBc = new TGraphErrors(n,x,AFBc,ex,dAFBc);
+  TGraphErrors* gAFBc_eR = new TGraphErrors(n,x,AFBc_eR,ex,dAFBc_eR);
+  TGraphErrors* gAFBq = new TGraphErrors(n,x,AFBq,ex,dAFBq);
+  TGraphErrors* gAFBq_eR = new TGraphErrors(n,x,AFBq_eR,ex,dAFBq_eR);
+  TGraphErrors* test= new TGraphErrors(n,x,AFBc,ex,dAFBc);
+  TGraphErrors* test2= new TGraphErrors(n,x,AFBc,ex,dAFBc);
+
+  TGaxis::SetMaxDigits(3);
+
+  TCanvas* c_AFBb = new TCanvas("c_AFBb","c_AFBb",800,800);
+  c_AFBb->cd(1);
+  // gPad->SetLogy();
+  gAFBb->GetXaxis()->SetTitle("E_{#gamma}^{cut}");
+  gAFBb->GetYaxis()->SetTitle("cross section [fb]");
+  gAFBb->GetYaxis()->SetRangeUser(500,30000);
+  gAFBb->SetLineColor(4);
+  gAFBb->SetLineWidth(2);
+  gAFBb->SetLineStyle(1);
+  gAFBb->Draw("al");
+
+  gAFBb_eR->SetLineColor(4);
+  gAFBb_eR->SetLineWidth(2);
+  gAFBb_eR->SetLineStyle(2);
+  gAFBb_eR->Draw("l");
+
+  gAFBc->SetLineColor(2);
+  gAFBc->SetLineWidth(2);
+  gAFBc->SetLineStyle(1);
+  gAFBc->Draw("l");
+
+  gAFBc_eR->SetLineColor(2);
+  gAFBc_eR->SetLineWidth(2);
+  gAFBc_eR->SetLineStyle(2);
+  gAFBc_eR->Draw("l");
+
+  gAFBq->SetLineColor(kGreen+1);
+  gAFBq->SetLineWidth(2);
+  gAFBq->SetLineStyle(1);
+  gAFBq->Draw("l");
+
+  gAFBq_eR->SetLineColor(kGreen+1);
+  gAFBq_eR->SetLineWidth(2);
+  gAFBq_eR->SetLineStyle(2);
+  gAFBq_eR->Draw("l");
+
+
+ test->SetLineColor(1);
+  test->SetLineWidth(2);
+  test->SetLineStyle(1);
+  test2->SetLineColor(1);
+  test2->SetLineWidth(2);
+  test2->SetLineStyle(2);
+
+  Labels("");
+
+
+  TString pol_string1 = "e_{L}^{-}e_{R}^{+}";
+  TString pol_string2 = "e_{R}^{-}e_{L}^{+}";
+
+  TString string1 = "";
+  TString string2 = "";
+
+  if(mode==1) {
+    pol_string1 = "e^{-}e^{+}";
+    pol_string2 = "e^{-}e^{+}";
+    string1 = ", P_{e^{-}e^{+}}=(-0.8,+0.3)";
+    string2 = ", P_{e^{-}e^{+}}=(+0.8,-0.3)";
+  }
+  
+  TLegend *leg_b = new TLegend(0.2,0.72,0.5,0.91);
+  leg_b->SetTextSize(0.035);
+  leg_b->SetTextFont(42);
+  leg_b->AddEntry(test,TString::Format("%s#rightarrow q#bar{q}%s",pol_string1.Data(),string1.Data()),"l");
+  leg_b->AddEntry(test2,TString::Format("%s#rightarrow q#bar{q}%s",pol_string2.Data(),string2.Data()),"l");
+  leg_b->AddEntry(gAFBb,"q=b","l");
+  leg_b->AddEntry(gAFBc,"q=c","l");
+  leg_b->AddEntry(gAFBq,"q=u,d,s","l"); 
+  leg_b->SetFillColor(0);
+  leg_b->SetLineColor(0);
+  leg_b->SetShadowColor(0);
+  leg_b->Draw();
+
+    
+  c_AFBb->Print(TString::Format("plots_draft/IncCrossSection_kcut_noacol_mode%i.eps",mode));
+
+}
+
 
 void Plots_Cross(int mode=0) {
 
@@ -642,7 +844,7 @@ void Plots_Cross(int mode=0) {
   TCanvas* c_AFBb = new TCanvas("c_AFBb","c_AFBb",800,800);
   c_AFBb->cd(1);
   // gPad->SetLogy();
-  gAFBb->GetXaxis()->SetTitle("K_{#gamma}-cut");
+  gAFBb->GetXaxis()->SetTitle("E_{#gamma}^{cut}");
   gAFBb->GetYaxis()->SetTitle("cross section [fb]");
   gAFBb->GetYaxis()->SetRangeUser(500,30000);
   gAFBb->SetLineColor(4);
@@ -695,8 +897,8 @@ void Plots_Cross(int mode=0) {
   if(mode==1) {
     pol_string1 = "e^{-}e^{+}";
     pol_string2 = "e^{-}e^{+}";
-    string1 = ", P(-80%,+30%)";
-    string2 = ", P(+80%,-30%)";
+    string1 = ", P_{e^{-}e^{+}}=(-0.8,+0.3)";
+    string2 = ", P_{e^{-}e^{+}}=(+0.8,-0.3)";
   }
   
   TLegend *leg_b = new TLegend(0.2,0.72,0.5,0.91);
@@ -882,7 +1084,7 @@ void Plots_Cross2(int mode=0) {
   test2->SetLineWidth(2);
   test2->SetLineStyle(2);
 
-  Labels(", K_{#gamma}<35 GeV");
+  Labels(", E_{#gamma}<35 GeV");
 
 
   TString pol_string1 = "e_{L}^{-}e_{R}^{+}";
@@ -894,8 +1096,8 @@ void Plots_Cross2(int mode=0) {
   if(mode==1) {
     pol_string1 = "e^{-}e^{+}";
     pol_string2 = "e^{-}e^{+}";
-    string1 = ", P(-80%,+30%)";
-    string2 = ", P(+80%,-30%)";
+    string1 = ", P_{e^{-}e^{+}}=(-0.8,+0.3)";
+    string2 = ", P_{e^{-}e^{+}}=(+0.8,-0.3)";
   }
   
   TLegend *leg_b = new TLegend(0.2,0.72,0.5,0.91);
@@ -1058,7 +1260,7 @@ void Plots_R(int mode=0) {
   TCanvas* c_AFBb = new TCanvas("c_AFBb","c_AFBb",800,800);
   c_AFBb->cd(1);
   // gPad->SetLogy();
-  gAFBb->GetXaxis()->SetTitle("K_{#gamma}-cut");
+  gAFBb->GetXaxis()->SetTitle("E_{#gamma}^{cut}");
   gAFBb->GetYaxis()->SetTitle("R");
   gAFBb->GetYaxis()->SetRangeUser(0,0.6);
   gAFBb->SetLineColor(4);
@@ -1084,19 +1286,33 @@ void Plots_R(int mode=0) {
 
   Labels(", sin(#Psi_{acol})<0.3");
   
-  TLegend *leg_b = new TLegend(0.2,0.5,0.5,0.7);
+   TString pol_string1 = "e_{L}^{-}e_{R}^{+}";
+  TString pol_string2 = "e_{R}^{-}e_{L}^{+}";
+
+  TString string1 = "";
+  TString string2 = "";
+
+  if(mode==1) {
+    pol_string1 = "e^{-}e^{+}";
+    pol_string2 = "e^{-}e^{+}";
+    string1 = ", P_{e^{-}e^{+}}=(-0.8,+0.3)";
+    string2 = ", P_{e^{-}e^{+}}=(+0.8,-0.3)";
+  }
+  
+  TLegend *leg_b = new TLegend(0.2,0.4,0.5,0.6);
   leg_b->SetTextSize(0.035);
   leg_b->SetTextFont(42);
-  leg_b->AddEntry(gAFBb,"e_{L}^{-}e_{R}^{+}#rightarrow b#bar{b}","l");
-  leg_b->AddEntry(gAFBb_eR,"e_{R}^{-}e_{L}^{+}#rightarrow b#bar{b}","l");
-  leg_b->AddEntry(gAFBc,"e_{L}^{-}e_{R}^{+}#rightarrow c#bar{c}","l");
-  leg_b->AddEntry(gAFBc_eR,"e_{R}^{-}e_{L}^{+}#rightarrow c#bar{c}","l");
+  leg_b->AddEntry(gAFBb,TString::Format("%s#rightarrow b#bar{b}%s",pol_string1.Data(),string1.Data()),"l");
+  leg_b->AddEntry(gAFBb_eR,TString::Format("%s#rightarrow b#bar{b}%s",pol_string2.Data(),string2.Data()),"l");
+  leg_b->AddEntry(gAFBc,TString::Format("%s#rightarrow c#bar{c}%s",pol_string1.Data(),string1.Data()),"l");
+  leg_b->AddEntry(gAFBc_eR,TString::Format("%s#rightarrow c#bar{c}%s",pol_string2.Data(),string2.Data()),"l");
   leg_b->SetFillColor(0);
   leg_b->SetLineColor(0);
   leg_b->SetShadowColor(0);
   leg_b->Draw();
 
-  c_AFBb->Print("plots_draft/R_kcut.eps");
+
+  c_AFBb->Print(TString::Format("plots_draft/R_kcut_mode%i.eps",mode));
 
 }
 
@@ -1243,7 +1459,7 @@ void Plots_R2(int mode=0) {
   TCanvas* c_AFBb = new TCanvas("c_AFBb","c_AFBb",800,800);
   c_AFBb->cd(1);
   // gPad->SetLogy();
-  gAFBb->GetXaxis()->SetTitle("sin(#Psi_{acol})-cut");
+  gAFBb->GetXaxis()->SetTitle("sin(#Psi_{acol})^{cut}");
   gAFBb->GetYaxis()->SetTitle("R");
   gAFBb->GetYaxis()->SetRangeUser(0,0.6);
   gAFBb->SetLineColor(4);
@@ -1267,21 +1483,36 @@ void Plots_R2(int mode=0) {
   gAFBc_eR->Draw("l");
 
 
-  Labels(", K_{#gamma}<35  GeV");
+  Labels(", E_{#gamma}<35  GeV");
   
-  TLegend *leg_b = new TLegend(0.2,0.5,0.5,0.7);
+    TString pol_string1 = "e_{L}^{-}e_{R}^{+}";
+  TString pol_string2 = "e_{R}^{-}e_{L}^{+}";
+
+  TString string1 = "";
+  TString string2 = "";
+
+  if(mode==1) {
+    pol_string1 = "e^{-}e^{+}";
+    pol_string2 = "e^{-}e^{+}";
+    string1 = ", P_{e^{-}e^{+}}=(-0.8,+0.3)";
+    string2 = ", P_{e^{-}e^{+}}=(+0.8,-0.3)";
+  }
+  
+  TLegend *leg_b = new TLegend(0.2,0.4,0.5,0.6);
   leg_b->SetTextSize(0.035);
   leg_b->SetTextFont(42);
-  leg_b->AddEntry(gAFBb,"e_{L}^{-}e_{R}^{+}#rightarrow b#bar{b}","l");
-  leg_b->AddEntry(gAFBb_eR,"e_{R}^{-}e_{L}^{+}#rightarrow b#bar{b}","l");
-  leg_b->AddEntry(gAFBc,"e_{L}^{-}e_{R}^{+}#rightarrow c#bar{c}","l");
-  leg_b->AddEntry(gAFBc_eR,"e_{R}^{-}e_{L}^{+}#rightarrow c#bar{c}","l");
+  leg_b->AddEntry(gAFBb,TString::Format("%s#rightarrow b#bar{b}%s",pol_string1.Data(),string1.Data()),"l");
+  leg_b->AddEntry(gAFBb_eR,TString::Format("%s#rightarrow b#bar{b}%s",pol_string2.Data(),string2.Data()),"l");
+  leg_b->AddEntry(gAFBc,TString::Format("%s#rightarrow c#bar{c}%s",pol_string1.Data(),string1.Data()),"l");
+  leg_b->AddEntry(gAFBc_eR,TString::Format("%s#rightarrow c#bar{c}%s",pol_string2.Data(),string2.Data()),"l");
   leg_b->SetFillColor(0);
   leg_b->SetLineColor(0);
   leg_b->SetShadowColor(0);
   leg_b->Draw();
 
-  c_AFBb->Print("plots_draft/R_acolcut.eps");
+
+
+  c_AFBb->Print(TString::Format("plots_draft/R_acolcut_mode%i.eps",mode));
 
 }
 
@@ -1358,7 +1589,7 @@ void Plots1(TString pol="eR_pL") {
   TCanvas* c_AFBb = new TCanvas("c_AFBb","c_AFBb",800,800);
   c_AFBb->cd(1);
   // gPad->SetLogy();
-  gAFBb->GetXaxis()->SetTitle("K_{#gamma-cut}");
+  gAFBb->GetXaxis()->SetTitle("E_{#gamma^{cut}}");
   gAFBb->GetYaxis()->SetTitle("AFB [%]");
   gAFBb->SetLineColor(4);
   gAFBb->SetLineWidth(2);
@@ -1478,7 +1709,7 @@ void Plots2(TString pol="eR_pL") {
   TCanvas* c_AFBb = new TCanvas("c_AFBb","c_AFBb",800,800);
   c_AFBb->cd(1);
   // gPad->SetLogy();
-  gAFBb->GetXaxis()->SetTitle("sin #Psi -cut");
+  gAFBb->GetXaxis()->SetTitle("sin #Psi ^{cut}");
   gAFBb->GetYaxis()->SetTitle("AFB [%]");
   gAFBb->SetLineColor(4);
   gAFBb->SetLineWidth(2);
@@ -1530,9 +1761,10 @@ void Plots2(TString pol="eR_pL") {
 void Observables() {
 
   //Plots_Cross();
-  for(int i=0; i<1; i++) {
+  for(int i=0; i<2; i++) {
     Plots_AFB(i);
     Plots_AFB2(i);
+    Plots_Cross0(i);
     Plots_Cross(i);
     Plots_Cross2(i);
     Plots_CrossDiff(i);
