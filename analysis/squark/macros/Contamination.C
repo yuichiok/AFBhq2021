e08dc94ff2a10c12c29e8d9224157bc693b353a1
airqui <airqui@ific.uv.es>
2022-10-20 16:30:34 +0200

    Pltos for presentation

--- analysis/squark/macros/Contamination.C
+++ analysis/squark/macros/Contamination.C
@@ -39,7 +39,7 @@ void Labels(TString pol, float luminosity=0){
 
 void ContaminationPlots(int isel=6, TString pol="eL_pR") {
 
-  TString quark_string[6] ={"","d","u","s","c","b"};
+  TString quark_string[7] ={"","d","u","s","c","b","radret"};
 
   SetQQbarStyle();
   gStyle->SetOptFit(0);
@@ -66,18 +66,32 @@ void ContaminationPlots(int isel=6, TString pol="eL_pR") {
     luminosity=h_Ntotal_nocuts->GetEntries()/cross_section[0][4];
     if(pol=="eR_pL") luminosity=h_Ntotal_nocuts->GetEntries()/cross_section[1][4];
 
-    TH1F*  h_AFB_reco[6];
-    TH1F*  h_AFB_true[6];
+    TH1F*  h_AFB_reco[7];
+    TH1F*  h_AFB_true[7];
     for(int q=1; q<6; q++) {
       h_AFB_true[q] = (TH1F*)f->Get(TString::Format("h_AFB_true_q%i_sel%i",q,isel));
       h_AFB_reco[q] = (TH1F*)f->Get(TString::Format("h_AFB_reco_q%i_sel%i",q,isel));
       h_AFB_true[q]->Sumw2();
       h_AFB_reco[q]->Sumw2();      
     }
-
+    filename = "../results/s_AFB_"+KID+"_2f_hadronic_"+pol+"_250GeV_radret.root";
+    f = new TFile(filename);
+    for(int q=1; q<6; q++) {
+      if(q==1) {
+        h_AFB_true[6] = (TH1F*)f->Get(TString::Format("h_AFB_true_q%i_sel%i",q,isel));
+        h_AFB_reco[6] = (TH1F*)f->Get(TString::Format("h_AFB_reco_q%i_sel%i",q,isel));
+      } else {
+        TH1F* temp1 = (TH1F*)f->Get(TString::Format("h_AFB_true_q%i_sel%i",q,isel));
+        TH1F* temp2 = (TH1F*)f->Get(TString::Format("h_AFB_reco_q%i_sel%i",q,isel));  
+        h_AFB_true[6]->Add(temp1);
+        h_AFB_reco[6]->Add(temp2);
+      }
+      h_AFB_true[6]->Sumw2();
+      h_AFB_reco[6]->Sumw2();      
+    }
 //---------------
     THStack *bkg_true = new THStack("bkg_true","");
-    for(int i=1; i<6; i++) {
+    for(int i=1; i<7; i++) {
       if(i!=3) bkg_true->Add(h_AFB_true[i]);
       h_AFB_true[i]->SetLineColor(i);
       h_AFB_true[i]->SetLineWidth(2);
@@ -104,19 +118,19 @@ void ContaminationPlots(int isel=6, TString pol="eL_pR") {
     Labels(pol,luminosity);
 
     TLegend *leg_true = new TLegend(0.2,0.7,0.4,0.9);//(0.4,0.3,0.5,0.6);
-    if(isel==9) leg_true->SetHeader("reco-level, cheat on K-ID, "+KID);
+    if(isel==8) leg_true->SetHeader("reco-level, cheat on K-ID, "+KID);
     leg_true->SetTextSize(0.035);
-    for(int q=1; q<6; q++) leg_true->AddEntry(h_AFB_true[q],TString::Format("%s, %s",quark_string[q].Data(),pol.Data()),"fl");
+    for(int q=1; q<7; q++) leg_true->AddEntry(h_AFB_true[q],TString::Format("%s, %s",quark_string[q].Data(),pol.Data()),"fl");
     leg_true->SetFillStyle(0);
     leg_true->SetLineWidth(0);
     leg_true->SetLineColor(0);
     leg_true->SetBorderSize(0);
     leg_true->Draw();
 
-    c_eff_MC_true->Print(TString::Format("c_cont_true_MC_sel%i_%s_%s.eps",isel,KID.Data(),pol.Data()));
+    c_eff_MC_true->Print(TString::Format("c_cont_true_MC_sel%i_%s_%s.pdf",isel,KID.Data(),pol.Data()));
 //--------------------------------
     THStack *bkg_reco = new THStack("bkg_reco","");
-    for(int i=1; i<6; i++) {
+    for(int i=1; i<7; i++) {
       if(i!=3) bkg_reco->Add(h_AFB_reco[i]);
       h_AFB_reco[i]->SetLineColor(i);
       h_AFB_reco[i]->SetLineWidth(2);
@@ -143,9 +157,9 @@ void ContaminationPlots(int isel=6, TString pol="eL_pR") {
     Labels(pol,luminosity);
 
     TLegend *leg_reco = new TLegend(0.2,0.7,0.4,0.9);//(0.4,0.3,0.5,0.6);
-    if(isel==9) leg_reco->SetHeader("reco-level, cheat on K-ID, "+KID);
+    if(isel==8) leg_reco->SetHeader("reco-level, cheat on K-ID, "+KID);
     leg_reco->SetTextSize(0.035);
-    for(int q=1; q<6; q++) leg_reco->AddEntry(h_AFB_reco[q],TString::Format("%s, %s",quark_string[q].Data(),pol.Data()),"fl");
+    for(int q=1; q<7; q++) leg_reco->AddEntry(h_AFB_reco[q],TString::Format("%s, %s",quark_string[q].Data(),pol.Data()),"fl");
     leg_reco->SetFillStyle(0);
     leg_reco->SetLineWidth(0);
     leg_reco->SetLineColor(0);
@@ -154,7 +168,7 @@ void ContaminationPlots(int isel=6, TString pol="eL_pR") {
 
 
   //c_eff_MC->Print(TString::Format("summaryplots/UnEff_sample_%i_pdg_%i_twopols.pdf",sample,iquark));
-    c_eff_MC_reco->Print(TString::Format("c_cont_reco_MC_sel%i_%s_%s.eps",isel,KID.Data(),pol.Data()));
+    c_eff_MC_reco->Print(TString::Format("c_cont_reco_MC_sel%i_%s_%s.pdf",isel,KID.Data(),pol.Data()));
 
   }
 }
