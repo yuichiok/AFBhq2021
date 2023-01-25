1a25d503e5e76e2cb83e7c8a66da11583a26e7b3
A. Irles <adrian.irles@ific.uv.es>
2023-01-25 14:58:54 +0100

    Preselection scripts

--- analysis/PreSelectionOpt/hadd2.sh
+++ analysis/PreSelectionOpt/hadd2.sh
@@ -1,5 +1,5 @@
 for pol in "eL_pR" "eR_pL"
 do
-    hadd -f -j 4 results/jettag_cuts6_2f_hadronic_sample_${pol}_250GeV.root output/jettag_cuts6_2f_hadronic_sample_${pol}_file_*_250GeV.root
-    hadd -f -j 4 results/jettag_cuts6_2f_hadronic_${pol}_250GeV.root output/jettag_cuts6_2f_hadronic_${pol}_file_*_250GeV.root
+    hadd -f -j 4 results/jettag_cuts5_2f_hadronic_sample_${pol}.root output/jettag_cuts5_2f_hadronic_sample_${pol}_file_*.root
+    hadd -f -j 4 results/jettag_cuts5_2f_hadronic_${pol}.root output/jettag_cuts5_2f_hadronic_${pol}_file_*.root
 done
