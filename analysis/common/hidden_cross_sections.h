a13e4204ed7236654882179a2ee48ea5d23967d7
A. Irles <adrian.irles@ific.uv.es>
2023-07-25 17:54:52 +0200

    correctionformat

--- analysis/common/hidden_cross_sections.h
+++ analysis/common/hidden_cross_sections.h
@@ -1,8 +1,8 @@
-std::vector<pair<TString,double> > cross_section
+std::vector<std::pair<TString,double> > cross_section_pairs
  {
-  ("qqSM",12200),
-  ("qqSMISR",48000),
-  ("HV_qv100GeV",119.9),
-  ("HV_qv50GeV",124.3),
-  ("HV_qv10GeV",124.4)
+  {"qqSM_unpol",12200},
+  {"qqSMISR_unpol",48000},
+  {"HV_qv100GeV_unpol",119.9},
+  {"HV_qv50GeV_unpol",124.3},
+  {"HV_qv10GeV_unpol",124.4}
   };
